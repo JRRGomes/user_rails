fa0f0110ea2e4646e9f4bae406c12e63f28f6eef
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-02 11:22:02 -0300

    feat: Contact model updated to validate Birthdate format to expect xx/xx/xx or xx-xx-xx

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :birthdate_before_type_cast, format: { with: %r/(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/ }
 end
