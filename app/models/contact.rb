0dd7d302aff38e2b35dd7aee7af46635a4082ed0
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-03 10:23:35 -0300

    fix: Contact birthdate validation fixed to accept nil values or a date format: xx/xx/xx or xx-xx-xx

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :user
   has_many :phones
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :user_id, message: 'Only different contacts name allowed for same user' }
-  validates :birthdate_before_type_cast, format: { with: %r/(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/ }
+  validates :birthdate_before_type_cast, format: { with: %r/(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/ }, allow_blank: true
 end
