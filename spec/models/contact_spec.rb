56ea4ebc66ced8958a9b12fcfc56e41156ae5817
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-04 14:45:32 -0300

    style: Context changed to become more semantic

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe Contact do
   describe 'validations' do
-    context 'when create new contact' do
+    context 'when create new contact with blank name' do
       it { should validate_presence_of(:name) }
+    end
+    context 'when create duplicated contacts for same user' do
       subject { Contact.new(name: 'someone') }
       it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:user_id).with_message('Only different contacts name allowed for same user') }
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe User do
   describe 'validations' do
-    context 'when create new user' do
+    context 'when create new user with blank name' do
       it { should validate_presence_of(:name) }
+    end
+    context 'when create duplicated users' do
       subject { User.new(name: 'someone') }
       it { should validate_uniqueness_of(:name).case_insensitive }
     end
