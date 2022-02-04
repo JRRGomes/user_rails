4616e6bdbc16ec7a183315637b620bf38fc4dfcc
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-04 14:13:29 -0300

    feat: shoulda-matchers installed for Rspec. Tests for User Model validations and associations created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'validations' do
+    context 'when create new user' do
+      it { should validate_presence_of(:name) }
+      subject { User.new(name: 'something') }
+      it { should validate_uniqueness_of(:name).case_insensitive }
+    end
+  end
+  describe 'associations' do
+    it { should have_many(:contacts) }
+  end
 end
