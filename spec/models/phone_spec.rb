06b0831210ec13d86653c85c4c38f731e70f3d7d
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-04 22:02:59 -0300

    feat: Test for validation of blank number for Phone Model

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Phone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Phone do
+  describe 'validations' do
+    context 'when create new phone with blank number' do
+      it { should validate_presence_of(:number) }
+    end
+    #context 'when create duplicated numbers for same contact' do
+     # subject { Phone.new(number: '123', kind: 'something') }
+     # it { should validate_uniqueness_of(:number).scoped_to(:contact_id) }
+    #end
+  end
 end
