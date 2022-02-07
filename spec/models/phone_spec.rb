6a6ff46635bb6b13dba8091ff4dcc191b7c8935c
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-07 14:33:18 -0300

    feat: Phone test validation for presence of Kind value

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe Phone do
   describe 'validations' do
-    context 'when create new phone with blank number' do
+    context 'when create new phone with blank number value' do
       it { should validate_presence_of(:number) }
     end
     context 'when create duplicated number for same contact' do
       subject { Phone.create(number: '123', kind: 'something', primary: false) }
       it { should validate_uniqueness_of(:number).scoped_to(:contact_id) }
+    end
+    context 'when create new phone with blank kind value' do
+      it { should validate_presence_of(:kind) }
     end
   end
 end
