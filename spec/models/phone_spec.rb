3869b9605f7bbcee5c4bf7677258532abef8e478
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-07 14:25:57 -0300

    feat: Contact test included to validate if a Birthdate is in wrong format

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -6,7 +6,7 @@ describe Phone do
       it { should validate_presence_of(:number) }
     end
     context 'when create duplicated number for same contact' do
-      subject { Phone.new(number: '123', kind: 'something', primary: false) }
+      subject { Phone.create(number: '123', kind: 'something', primary: false) }
       it { should validate_uniqueness_of(:number).scoped_to(:contact_id) }
     end
   end
