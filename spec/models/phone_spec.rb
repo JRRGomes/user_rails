e5842dce163e2733faad7b103ed75252c259ed0f
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-07 08:45:49 -0300

    feat: phone spec updated to include test for duplicated number for the same contact id

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -5,9 +5,9 @@ describe Phone do
     context 'when create new phone with blank number' do
       it { should validate_presence_of(:number) }
     end
-    #context 'when create duplicated numbers for same contact' do
-     # subject { Phone.new(number: '123', kind: 'something') }
-     # it { should validate_uniqueness_of(:number).scoped_to(:contact_id) }
-    #end
+    context 'when create duplicated number for same contact' do
+      subject { Phone.new(number: '123', kind: 'something', primary: false) }
+      it { should validate_uniqueness_of(:number).scoped_to(:contact_id) }
+    end
   end
 end
