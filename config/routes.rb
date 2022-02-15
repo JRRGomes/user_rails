1e131c4450340f28b180d9cfb3f48be9f55e758c
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-15 16:55:47 -0300

    feat: Created route to get users/:id and show users info on users/show. Added a collumn with links to show each user on Index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users' => 'users#index'
-  get 'users/show'
+  get 'users/show' => 'users#show'
+  get 'users/:id' => 'users#show', as: :user
   get 'users/new'
   post 'users/create'
   post 'users/edit'
