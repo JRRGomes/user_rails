f346b63752a70281085f87c7bb9027caf3bca55a
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-16 15:51:49 -0300

    feat: Created div field to display error message when creating new user without name. Also created a link_to new user on index.html. Fixed the route to post new user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/show' => 'users#show'
   get 'users/new' => 'users#new'
-  post 'users/create'
+  post 'users' => 'users#create'
   post 'users/edit'
   post 'users/update'
   delete 'users/destroy'
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Users", type: :request do
 
   describe "POST /create" do
     it "returns http success" do
-      post "/users/create"
+      post "/users#create"
       expect(response).to have_http_status(:success)
     end
   end
