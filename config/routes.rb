b8c5ee32acd67eb3724d0a74b6ba6b1c35c863b1
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-16 11:41:54 -0300

    style: Changed users/:id position to avoid problems on other get routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/show' => 'users#show'
-  get 'users/:id' => 'users#show', as: :user
   get 'users/new' => 'users#new'
   post 'users/create'
   post 'users/edit'
   post 'users/update'
   delete 'users/destroy'
+  get 'users/:id' => 'users#show', as: :user
 end
