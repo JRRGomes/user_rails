5dd887b082bc8d1af59ec50432c46562eeed1fd5
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-15 21:41:07 -0300

    feat: Test created to expect success http response to user#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/show' => 'users#show'
   get 'users/:id' => 'users#show', as: :user
-  get 'users/new'
+  get 'users/new' => 'users#new'
   post 'users/create'
   post 'users/edit'
   post 'users/update'
